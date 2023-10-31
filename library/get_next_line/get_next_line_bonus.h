866728ac464c97007858d9c13048b8f3e249ba04
Lea Yeh <lyeh@c2r2p6.42vienna.com>
2023-10-31 18:25:04 +0100

    chore: Modify the buffer size of gnl

--- library/get_next_line/get_next_line_bonus.h
+++ library/get_next_line/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 14:48:11 by lyeh              #+#    #+#             */
-/*   Updated: 2023/09/26 16:08:19 by lyeh             ###   ########.fr       */
+/*   Updated: 2023/10/31 18:20:53 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <sys/resource.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 128
 # endif
 
 # ifndef MAX_FD
